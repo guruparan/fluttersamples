ac796ab17f4570c92321993c162071dbf82b2b0b
Guruparan Giritharan <guru_sig@yahoo.com>
2019-07-14 00:08:14 +0530

    Updated movie service

--- moviesapp/lib/MovieService.dart
+++ moviesapp/lib/MovieService.dart
@@ -1,11 +1,12 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 import 'package:moviesapp/models/Movie.dart';
 
+const APIKEY = "<Your api key here>";
+
 Future<List<Movie>> searchMovies(keyword) async {
   final response =
-      await http.get('http://www.omdbapi.com/?apikey=53473bbd&s=' + keyword);
+      await http.get('http://www.omdbapi.com/?apikey=$APIKEY&s=$keyword');
 
   if (response.statusCode == 200) {
     Map data = json.decode(response.body);
@@ -15,6 +16,22 @@ Future<List<Movie>> searchMovies(keyword) async {
           .map((item) => new Movie.fromJson(item))
           .toList();
       return list;
+    } else {
+      throw Exception(data['Error']);
+    }
+  } else {
+    throw Exception('Something went wrong !');
+  }
+}
+
+Future<Movie> getMoviw(movieId) async {
+  final response =
+      await http.get('http://www.omdbapi.com/?apikey=$APIKEY&i=$movieId');
+
+  if (response.statusCode == 200) {
+    Map data = json.decode(response.body);
+    if (data['Response'] == "True") {
+      return null;
     } else {
       throw Exception(data['Error']);
     }
